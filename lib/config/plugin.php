934e5654718421e212e510ee079d80f92c828af5
Pavel <pvl@shutoff.ru>
2016-11-30 09:54:14 +0300

    version  1.3.0

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -2,7 +2,7 @@
 return array (
   'name' => 'Скрытые настройки',
   'img' => 'img/hidset.png',
-  'version' => '1.2.0',
+  'version' => '1.3.0',
   'vendor' => '834834',
   'custom_settings' => true,
 );
